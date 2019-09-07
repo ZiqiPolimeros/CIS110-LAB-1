e350d91834a2b13175681b7863b777278447f00e
Ziqi Wu <55024117+ZiqiPolimeros@users.noreply.github.com>
2019-09-07 13:55:14 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+package helloworld;
+
+public class HelloWorld {  
+    public static void main(String[] args) {
+           System.out.println("The truth shall set you free!\n\t\" I am a new line.\"");
+           System.out.println("Good Luck!");
+           System.out.print("I am a new line");
+           System.out.print("I am a new line, I am not a new line");
+           System.out.println();
+           System.out.println("I am the first string."+"I am the second string");
+           System.out.println(
+                   "This is my new creenply"+
+                   "by professor Conroy \n"+
+                   "\n"+
+                   "The Best Screenplay Ever Written...\n"+
+                   "Eveen Better than Star Wars...\n"+
+                   "\t written in "+ 1978
+                   );
+    }
+}
