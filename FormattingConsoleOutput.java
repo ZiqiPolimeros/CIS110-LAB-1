35cd1be98df0b0e2c927c034971d5ebe56bb470b
Ziqi Wu <55024117+ZiqiPolimeros@users.noreply.github.com>
2019-09-14 13:57:02 -0400

    Add files via upload

---
+++ FormattingConsoleOutput.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FormattingConsoleOutput;
+
+/**
+ *
+ * @author ziqi.wu
+ */
+public class FormattingConsoleOutput {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("								O'Malley says,"+" \"Practice\\ makes\\ perfect!\"");
+        System.out.println("");
+        System.out.println("					Now,"+" \\\\\" without further adeiu, a poem inside a frame:");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("@");
+        System.out.println("@                          r-p-o-p-h-e-s-s-a-g-r");
+        System.out.println("@                       E. E. Cummings - 1894-1962");
+        System.out.println("@");
+        System.out.println("@");
+        System.out.println("@");
+        System.out.println("@                                     r-p-o-p-h-e-s-s-a-g-r");
+        System.out.println("@                           who");
+        System.out.println("@  a)s w(e loo)k");
+        System.out.println("@  upnowgath");
+        System.out.println("@                       PPEGORHRASS");
+        System.out.println("@                                                       eringint(o-");
+        System.out.println("@  aThe):l");
+        System.out.println("@               eA");
+        System.out.println("@                    !p:");
+        System.out.println("@ S                                                                        a");
+        System.out.println("@                                     (r");
+        System.out.println("@ rIvInG                              .gRrEaPsPhOs)");
+        System.out.println("@                                                                        to"); 
+        System.out.println("@ rea(be)rran(com)gi(e)ngly");
+        System.out.println("@ ,grasshopper;");
+        System.out.println("@");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+      
+    }
+}
