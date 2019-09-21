f25072e1d3e756977973f9f0731be35af66ad70a
Ziqi Wu <55024117+ZiqiPolimeros@users.noreply.github.com>
2019-09-21 16:51:41 -0400

    Add files via upload

--- Variables.java
+++ Variables.java
@@ -10,40 +10,39 @@ package variables;
  * @author Ziqi Wu
  */
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Variables {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) {       
         System.out.println("//SAMPLE OUTPUT");
         System.out.println("Welcome to the mass calculator, what is your name?");
         Scanner scan=new Scanner(System.in);
         String x;
-        //define x
-        int y;
-        //define y
-        int z;
-        //define z
+        //define x    
        
-        x=scan.nextLine();
+       x=scan.nextLine();
         //nextLine= input letters.
         System.out.println("Hi "+x+"! How old are you?");
-        y=scan.nextInt();
+        int y=scan.nextInt();
         System.out.println("My, you're getting younger and younger! You sure are looking good for your age! Would you be so kind as to tell me your weight? (on Earth please!)");
-        z=scan.nextInt();
+        int z=scan.nextInt();
         System.out.println("Let me do a bit of analysis...");
         System.out.println("");
         System.out.println("All done! Here are the results:");
         System.out.println("");
-        System.out.println("Earth:        "+z);
+        DecimalFormat twoDForm = new DecimalFormat("#.00");
+        System.out.println("Earth:        "+twoDForm.format(z));
         double a=z*0.16;
-        System.out.println("The Moon:      "+a);
+        System.out.println("The Moon:      "+twoDForm.format(a));
         double b=z*0.37;
-        System.out.println("Mars:          "+b);
+        System.out.println("Mars:          "+twoDForm.format(b));
         double c=z*2.364;
-        System.out.println("Jupiter:      "+c);
+        System.out.println("Jupiter:      "+twoDForm.format(c));
         System.out.println("");
         System.out.println("Phew! That was a work-out! Please come back again sometime!");
         
